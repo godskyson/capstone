a8c87776c8d3880f85b6f807b45234831e48ba53
godskyson <king03603@kakao.com>
2024-10-29 16:07:48 +0900

    Update editor.py

--- editor.py
+++ editor.py
@@ -27,9 +27,10 @@ def register_user(username, password):
 
 # 게시글 작성 함수
 def create_post(username, title, content):
-    c.execute('INSERT INTO posts (username, title, content) VALUES (?, ?, ?)', (username, title, content))
-    conn.commit()
-    st.success(f"게시물 '{title}'이(가) 저장되었습니다!")
+    try:
+        c.execute('INSERT INTO posts (username, title, content) VALUES (?, ?, ?)', (username, title, content))
+        conn.commit()W
+        st.success(f"게시물 '{title}'이(가) 저장되었습니다!")
     except Exception as e:
         st.error(f"게시물 저장 중 오류 발생: {e}")
 
